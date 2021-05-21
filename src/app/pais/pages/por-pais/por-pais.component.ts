5eded70c5c28c92946f696fdbe3cfa854390f038
meana <ramon.meana@gmail.com>
2021-05-21 19:05:18 +0200

    response typing & fill countries table

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,6 +12,7 @@ export class PorPaisComponent  {
 
   termino: string = ''
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
@@ -20,8 +22,10 @@ export class PorPaisComponent  {
     this.paisService.buscarPais(this.termino)
       .subscribe(resp => {
         console.log(resp);
+        this.paises = resp;
       }, err => {
         this.hayError = true;
+        this.paises = []
       })
   }
 }
