bc7386dbfcc54b1c8c24931403d2324a97d02af1
meana <ramon.meana@gmail.com>
2021-05-21 18:22:27 +0200

    Country component

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent  {
+
+  termino: string = ''
 
   constructor() { }
 
-  ngOnInit(): void {
+  buscar(){
+    console.log(this.termino)
   }
-
 }
